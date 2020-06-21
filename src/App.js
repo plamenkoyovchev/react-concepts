5875beaaa64468835a9f4e3e7a2e7bd69a779887
Plamen Yovchev <plamenkooo@gmail.com>
2020-06-21 11:07:45 +0300

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Counter from "./patterns/set-state/Counter";
+import ClickCounter from "./patterns/hoc/ClickCounter";
 
 const App = () => {
   return (
     <div className="App">
-      <Counter />
+      <div>
+        <h4>Normal counter implementation</h4>
+        <Counter />
+      </div>
+      <div>
+        <h4>Shared functionality with withCounter HOC component</h4>
+        <ClickCounter name="Plamen" />
+      </div>
     </div>
   );
 };
