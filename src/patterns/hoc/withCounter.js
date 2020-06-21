259804ecd8da669867be21657e5f2c6d8351bbab
Plamen Yovchev <plamenkooo@gmail.com>
2020-06-21 10:58:37 +0300

    Implement withCounter hoc

---
+++ src/patterns/hoc/withCounter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+const withCounter = (WrappedComponent) => {
+  class WithCounter extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        count: 0,
+      };
+    }
+
+    increment = () => {
+      this.setState((prevState) => {
+        return {
+          count: prevState.count + 1,
+        };
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent count={this.state.count} increment={this.increment} />
+      );
+    }
+  }
+
+  return WithCounter;
+};
+
+export default withCounter;
