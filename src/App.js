7740123a3536d1341505cf8d8281b1d4fae7e25f
Plamen Yovchev <plamenkooo@gmail.com>
2020-06-21 20:00:13 +0300

    pass function as prop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Counter from "./patterns-and-concepts/set-state/Counter";
 import ClickCounter from "./patterns-and-concepts/hoc/ClickCounter";
 import HoverCounter from "./patterns-and-concepts/hoc/HoverCounter";
+import User from "./patterns-and-concepts/render-props/User";
 
 const App = () => {
   return (
@@ -15,6 +16,10 @@ const App = () => {
         <h4>Shared functionality with withCounter HOC component</h4>
         <ClickCounter name="Plamen" />
         <HoverCounter />
+      </div>
+      <div>
+        <h4>Render props pattern</h4>
+        <User render={(isLoggedIn) => (isLoggedIn ? "Plamen" : "Guest")} />
       </div>
     </div>
   );
