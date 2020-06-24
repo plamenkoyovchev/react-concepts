5483cc94f0b4be49e27167369eb8c8890e38011f
plamenyovchev <plamenkooo@gmail.com>
2020-06-24 21:13:06 +0300

    Add basic form handling

--- src/patterns-and-concepts/forms/Form.jsx
+++ src/patterns-and-concepts/forms/Form.jsx
@@ -1,5 +1,45 @@
 import React, { Component } from "react";
 
-class Form extends Component {}
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      gender: "",
+      age: "",
+    };
+  }
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("state", this.state);
+  };
+
+  onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitHandler}>
+        <input
+          type="text"
+          name="name"
+          value={this.state.value}
+          onChange={this.onChangeHandler}
+        />
+        <input
+          type="text"
+          name="age"
+          value={this.state.age}
+          onChange={this.onChangeHandler}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 export default Form;
