2c5ef07ab77e958cccf2ea806f98d805318873a7
plamenyovchev <plamenkooo@gmail.com>
2020-06-25 15:50:20 +0300

    Add LifecycleA component

---
+++ src/patterns-and-concepts/lifecycle-methods/mounting/LifecycleA.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class LifecycleA extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Plamen",
+    };
+
+    console.log("Lifecycle A constructor");
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("Lifecycle A getDerivedStateFromProps");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("Lifecycle A componentDidMount");
+  }
+
+  render() {
+    console.log("Lifecycle A render");
+    return <div>Lifecycle A</div>;
+  }
+}
+
+export default LifecycleA;
