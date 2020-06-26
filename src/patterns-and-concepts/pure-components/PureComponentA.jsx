72daa89d3234b85b1e315fef4f810ae9587657fb
Plamen Yovchev <plamenkooo@gmail.com>
2020-06-26 15:45:35 +0300

    Add PureComponentA

---
+++ src/patterns-and-concepts/pure-components/PureComponentA.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class PureComponentA extends PureComponent {
+  render() {
+    return <div>Pure Component A</div>;
+  }
+}
+
+export default PureComponentA;
