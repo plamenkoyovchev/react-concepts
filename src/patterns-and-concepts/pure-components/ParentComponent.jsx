a5a5ccff245f025db09591b44b048b4a8c29a15e
plamenyovchev <plamenkooo@gmail.com>
2020-06-27 21:16:11 +0300

    Add notes for PureComponent

--- src/patterns-and-concepts/pure-components/ParentComponent.jsx
+++ src/patterns-and-concepts/pure-components/ParentComponent.jsx
@@ -2,6 +2,9 @@ import React, { PureComponent } from "react";
 import RegularComponent from "./RegularComponent";
 import PureComponentA from "./PureComponentA";
 
+// It is considered as a good practice all children components also to extend PureComponent
+// if the parent extends PureComponent
+
 export class ParentComponent extends PureComponent {
   constructor(props) {
     super(props);
