1050e7ce637cd45554e1e45418bd257d8ae0719a
plamenyovchev <plamenkooo@gmail.com>
2020-07-04 12:46:37 +0300

    Add input field

--- src/hooks/effect/HooksCounter.jsx
+++ src/hooks/effect/HooksCounter.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 const HooksCounter = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     document.title = `Count: ${count}`;
@@ -10,6 +11,13 @@ const HooksCounter = () => {
 
   return (
     <>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Increment
       </button>
