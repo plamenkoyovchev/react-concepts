3e82451574d00963eee8f7f12be168aa037bea7c
plamenyovchev <plamenkooo@gmail.com>
2020-07-17 20:15:52 +0300

    Refactoring

--- src/hooks/data-fetching/DataFetchingWithUseReducer.jsx
+++ src/hooks/data-fetching/DataFetchingWithUseReducer.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
-import { useReducer } from "react";
-import { useEffect } from "react";
 
 const initialState = {
   post: {},
